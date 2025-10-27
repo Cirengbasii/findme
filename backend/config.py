e30d8f69c313671f98bc8e0db0b803c24ac964ef
destyaayud31 <destyaayud31@gmail.com>
2025-10-27 13:55:37 +0700

    update backend

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,6 @@ class Config:
     PG_HOST = "localhost"   
     PG_PORT = "5432"      
     PG_USER = "postgres"         
-    PG_PASSWORD = "Nopnop04"
+    PG_PASSWORD = "311204"
     PG_DB = "findme_db"          
     SECRET_KEY = "supersecretkey"
